b92e67f3e305a048bfd7ca58c5d037d6e6368433
Reto Brunner <reto@labrat.space>
2022-01-23 13:01:08 +0100

    Remove invalid documentation
    Leftover from the copy paste of the example post,
    not applicable to this source

--- neo-bot.py
+++ neo-bot.py
@@ -1,28 +1,5 @@
 #! /usr/bin/env python
-#
-# Example program using irc.bot.
-#
 # Joel Rosdahl <joel@rosdahl.net>
-"""A simple example bot.
-
-This is an example bot that uses the SingleServerIRCBot class from
-irc.bot.  The bot enters a channel and listens for commands in
-private messages and channel traffic.  Commands in channel messages
-are given by prefixing the text by the bot name followed by a colon.
-It also responds to DCC CHAT invitations and echos data sent in such
-sessions.
-
-The known commands are:
-
-    stats -- Prints some channel information.
-
-    disconnect -- Disconnect the bot.  The bot will try to reconnect
-                  after 60 seconds.
-
-    die -- Let the bot cease to exist.
-
-    dcc -- Let the bot invite you to a DCC CHAT connection.
-"""
 
 import irc.bot
 import irc.strings
